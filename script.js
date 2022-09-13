9851d7a7e594e87b9e9d88222affbbdf863337ea
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-09-12 22:59:43 -0300

    update

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-// const modalWrapper = document.querySelector('.modal-wrapper')
-// const modalMessage = document.querySelector('.modal .title span')
-// const messageText = document.querySelector('.message-text')
-// const modalBtnClose = document.querySelector('.modal button.close')
-
 const Modal = {
   wrapper: document.querySelector('.modal-wrapper'),
   message: document.querySelector('.modal .title span'),
